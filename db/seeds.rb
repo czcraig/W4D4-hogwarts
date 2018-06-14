c562fc33ae8905ee516ebfdb6bcf42470dd78794
James Henderson <hendersonjamesalexander@gmail.com>
2018-06-14 12:27:51 +0100

    Parts 1 and 2 finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 require_relative('../models/students')
+require( 'pry-byebug' )
 
-Student.delete_all()
+# Student.delete_all()
 
 student1 = Student.new({
   "first_name" => "Harry",
@@ -18,3 +19,6 @@ student2 = Student.new({
 
 student1.save
 student2.save
+
+binding.pry
+nil
