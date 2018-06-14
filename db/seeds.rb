3226f6b14c3004cbe8ae1290a39aff6f346c84bd
James Henderson <hendersonjamesalexander@gmail.com>
2018-06-14 12:18:54 +0100

    basic tests passed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,19 +2,19 @@ require_relative('../models/students')
 
 Student.delete_all()
 
-pizza1 = PizzaOrder.new({
-  "first_name" => "Walter",
-  "last_name" => "White",
-  "topping" => "Pepperoni",
-  "quantity" => 1
+student1 = Student.new({
+  "first_name" => "Harry",
+  "last_name" => "Potter",
+  "house" => "Gryffindor",
+  "age" => 17
 })
 
-pizza2 = PizzaOrder.new({
-  "first_name" => "Jesse",
-  "last_name" => "Pinkman",
-  "topping" => "Italian Sausage",
-  "quantity" => 12
+student2 = Student.new({
+  "first_name" => "Draco",
+  "last_name" => "Malfoy",
+  "house" => "Slytherin",
+  "age" => 17
 })
 
-pizza1.save
-pizza2.save
+student1.save
+student2.save
---
+++ models/specs/students_spec.rb
@@ -0,0 +1,32 @@
+require("minitest/autorun")
+require_relative("../students.rb")
+
+class TestStudent < MiniTest::Test
+
+  def setup
+    options = {"id" => 1, "first_name" => "Harry", "last_name" => "Potter",
+      "house" => "Gryffindor", "age" => 17}
+    @student = Student.new(options)
+  end
+
+  def test_first_name()
+    result = @student.first_name()
+    assert_equal("Harry", result)
+  end
+
+  def test_last_name()
+    result = @student.last_name()
+    assert_equal("Potter", result)
+  end
+
+  def test_house()
+    result = @student.house()
+    assert_equal("Gryffindor", result)
+  end
+
+  def test_age()
+    result = @student.age()
+    assert_equal(17, result)
+  end
+
+end
