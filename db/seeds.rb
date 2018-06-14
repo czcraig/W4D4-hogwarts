7747a82523c552595a7fde3861f15a06d27a617c
James Henderson <hendersonjamesalexander@gmail.com>
2018-06-14 13:35:15 +0100

    Part 3 finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 require_relative('../models/students')
 require( 'pry-byebug' )
 
-# Student.delete_all()
+Student.delete_all()
 
 student1 = Student.new({
   "first_name" => "Harry",
--- models/students.rb
+++ models/students.rb
@@ -34,4 +34,24 @@ class Student
     return "#{@first_name} #{@last_name}"
   end
 
+  def self.delete_all()
+    sql = "DELETE FROM students;"
+    SqlRunner.run(sql)
+  end
+
+  def self.all()
+    sql = "SELECT * FROM students"
+    students = SqlRunner.run( sql )
+    result = students.map { |student| Student.new( student ) }
+    return result
+  end
+
+  def self.find( id )
+    sql = "SELECT * FROM students WHERE id = $1"
+    values = [id]
+    student = SqlRunner.run( sql, values )
+    result = Student.new( student.first )
+    return result
+  end
+
 end
